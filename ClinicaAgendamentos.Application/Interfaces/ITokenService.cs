9b5e215970dd0f294bfaf75bb26ebf756ce3a904
Alecsandro <alex247417@hotmail.com>
2026-06-05 14:07:46 -0300

    feat(application): implementar interface de token e caso de uso de login

---
+++ ClinicaAgendamentos.Application/Interfaces/ITokenService.cs
@@ -0,0 +1,8 @@
+﻿using ClinicaAgendamentos.Domain.Entities;
+
+namespace ClinicaAgendamentos.Application.Interfaces;
+
+public interface ITokenService
+{
+    string GerarToken(Usuario usuario);
+}
---
+++ ClinicaAgendamentos.Application/UseCases/Auth/LoginUseCase.cs
@@ -0,0 +1,27 @@
+﻿using ClinicaAgendamentos.Domain.Interfaces;
+using ClinicaAgendamentos.Application.Interfaces;
+
+namespace ClinicaAgendamentos.Application.UseCases.Auth;
+
+public class LoginUseCase
+{
+    private readonly IUsuarioRepository _usuarioRepository;
+    private readonly ITokenService _tokenService;
+
+    public LoginUseCase(IUsuarioRepository usuarioRepository, ITokenService tokenService)
+    {
+        _usuarioRepository = usuarioRepository;
+        _tokenService = tokenService;
+    }
+
+    public async Task<string> ExecutarAsync(string email, string senha)
+    {
+        var usuario = await _usuarioRepository.ObterPorEmailAsync(email);
+        
+        // Simulação de validação de senha (em produção usaria BCrypt ou similar)
+        if (usuario == null || usuario.SenhaHash != senha) 
+            throw new Exception("Usuário ou senha inválidos.");
+
+        return _tokenService.GerarToken(usuario);
+    }
+}
